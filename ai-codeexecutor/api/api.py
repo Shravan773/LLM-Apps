b30a3ee4bcf4ad4d047ff3f7c8a33b9857dbe70e
Your Name <you@example.com>
2023-10-11 00:36:30 +0530

    Added error handling conditions to avoid execption

--- ai-codeexecutor/api/api.py
+++ ai-codeexecutor/api/api.py
@@ -2,32 +2,29 @@
 import requests
 import json
 
-REPLIT_API_URL = "https://repl.it/api/v1/execute"
-REPLIT_API_KEY = "YOUR_REPLIT_API_KEY"  # Replace this with your Repl.it API key
+API_BASE_URL = "https://tgapi-7d0b0583d985.herokuapp.com/api/v1/coderunner/execute"  # Replace this with the actual API endpoint URL
 
-def execute_python_code(code: str) -> str:
+def execute_gpt_code(code: str) -> str:
     """
-    Execute Python code using Repl.it's API.
+    Execute Python code using the GPT model via the API endpoint.
     
     Args:
         code (str): Python code to be executed.
     
     Returns:
         str: Execution result from the API.
-    """
+    """a
     try:
-        response = requests.post(REPLIT_API_URL, 
-                                 data={"language": "python3", "code": code}, 
-                                 headers={"Authorization": f"Bearer {REPLIT_API_KEY}"})
+        response = requests.post(API_BASE_URL, json={"code": code})
         response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
 
         try:
             api_response = response.json()
-            if 'output' in api_response:
-                result = api_response['output']
+            if 'result' in api_response:
+                result = api_response['result']
                 return result
             else:
-                return "Error: Unable to get output from the API response."
+                return "Error: 'result' key not found in the API response."
         except json.JSONDecodeError:
             return "Error: Invalid JSON response from the API."
 
@@ -41,6 +38,6 @@ if __name__ == "__main__":
         return "Hello, World!"
     print(hello())
     """
-    execution_result = execute_python_code(python_code)
+    execution_result = execute_gpt_code(python_code)
     print("Execution Result:")
     print(execution_result)
