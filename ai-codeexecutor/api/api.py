a87d1d236ee37397d7e9b4940513eb6ad0ffc665
Your Name <you@example.com>
2023-10-11 00:30:59 +0530

    Added error handling conditions to avoid exception

--- ai-codeexecutor/api/api.py
+++ ai-codeexecutor/api/api.py
@@ -1,12 +1,13 @@
 # api.py
 import requests
+import json
 
+REPLIT_API_URL = "https://repl.it/api/v1/execute"
+REPLIT_API_KEY = "YOUR_REPLIT_API_KEY"  # Replace this with your Repl.it API key
 
-
-API_BASE_URL = "https://tgapi-7d0b0583d985.herokuapp.com/api/v1/coderunner/execute"  # Replace this with the actual API endpoint URL
-def execute_gpt_code(code: str) -> str:
+def execute_python_code(code: str) -> str:
     """
-    Execute Python code using the GPT model via the API endpoint.
+    Execute Python code using Repl.it's API.
     
     Args:
         code (str): Python code to be executed.
@@ -15,10 +16,21 @@ def execute_gpt_code(code: str) -> str:
         str: Execution result from the API.
     """
     try:
-        response = requests.post(API_BASE_URL, json={"code": code})
+        response = requests.post(REPLIT_API_URL, 
+                                 data={"language": "python3", "code": code}, 
+                                 headers={"Authorization": f"Bearer {REPLIT_API_KEY}"})
         response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
-        result = response.json()["result"]
-        return result
+
+        try:
+            api_response = response.json()
+            if 'output' in api_response:
+                result = api_response['output']
+                return result
+            else:
+                return "Error: Unable to get output from the API response."
+        except json.JSONDecodeError:
+            return "Error: Invalid JSON response from the API."
+
     except requests.exceptions.RequestException as e:
         return f"Error occurred: {e}"
 
@@ -29,6 +41,6 @@ if __name__ == "__main__":
         return "Hello, World!"
     print(hello())
     """
-    execution_result = execute_gpt_code(python_code)
+    execution_result = execute_python_code(python_code)
     print("Execution Result:")
     print(execution_result)
